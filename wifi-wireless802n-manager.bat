e7900f4c00fb8f33fb3586fb7d5068a49c683b4a
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2019-12-20 23:12:15 -0300

    Update bat archive

--- wifi-wireless802n-manager.bat
+++ wifi-wireless802n-manager.bat
@@ -1,1 +1,7 @@
-java -jar build/libs/wife-wireless802n-manager-1.0-SNAPSHOT.jar "YOUR_SSID_HERE"
+@echo off
+set JAVA_HOME "C:\Users\PATH_TO_JDK_OR_JRE"
+set PATH "%PATH%;%JAVA_HOME%\bin";
+echo Display java version
+java -version
+java -jar build/libs/wifi-wireless802n-manager-1.0-SNAPSHOT-all.jar "YOUR_SSID_HERE"
+ping 127.0.0.1 -n 60 > nul
